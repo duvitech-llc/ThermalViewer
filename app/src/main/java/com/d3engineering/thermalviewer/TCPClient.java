537cba6817411639dedb97315ae05810794b10b2
George Vigelette <gvigelet@duvitech.com>
2015-10-26 01:18:12 -0400

    Added TCPClient

--- app/src/main/java/com/d3engineering/thermalviewer/TCPClient.java
+++ app/src/main/java/com/d3engineering/thermalviewer/TCPClient.java
@@ -15,7 +15,7 @@ import java.net.Socket;
  */
 public class TCPClient {
     private String serverMessage;
-    public static final String SERVERIP = "192.168.0.102"; //your computer IP address
+    public static final String SERVERIP = "192.168.0.102"; //D3 Stack IP Address
     public static final int SERVERPORT = 4444;
     private OnMessageReceived mMessageListener = null;
     private boolean mRun = false;
